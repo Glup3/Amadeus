848670c675cd6326d08b51b5eb2cbf452ae2fe2c
Glup3 <glupstar312@gmail.com>
2018-11-01 22:56:55 +0100

    add record audio permission add test image and textview add speechrecognizer
    Signed-off-by: Glup3 <glupstar312@gmail.com>

--- app/src/main/java/at/tra/glup3/amadeus/MainActivity.java
+++ app/src/main/java/at/tra/glup3/amadeus/MainActivity.java
@@ -2,16 +2,27 @@ package at.tra.glup3.amadeus;
 
 import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtAnzeige;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtAnzeige = findViewById(R.id.anzeige_Text);
     }
 
     @Override
@@ -19,4 +30,35 @@ public class MainActivity extends AppCompatActivity {
         super.attachBaseContext(LanguageContextWrapper.wrap(context));
     }
 
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+
+        //TODO 420 auslagern
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 420);
+        }
+        else {
+            Toast.makeText(this, "Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 420:
+                if(resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtAnzeige.setText(result.get(0));
+                }
+                break;
+        }
+    }
 }
